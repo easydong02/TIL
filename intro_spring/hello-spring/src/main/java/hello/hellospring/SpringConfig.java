45dcfb3838a76aff92c8c827b6c0948166412d1e
easydong02 <easydong02@gmail.com>
2022-11-06 00:17:57 +0900

    spring 1106

--- intro_spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ intro_spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,31 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository()); // ctrl + p 하면 어떤 것을 인자로 넣어줘야 하는지 알려준다.
+        return new MemberService(memberRepository); // ctrl + p 하면 어떤 것을 인자로 넣어줘야 하는지 알려준다.
     }
 
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 
 
 
