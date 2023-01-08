7b682f3a67bb73c821d8650253ea953203497946
easydong02 <easydong02@gmail.com>
2023-01-08 23:12:52 +0900

    [2023-01-08] fix memberformcontrollerV2

--- inflearn/spring_mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ inflearn/spring_mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class MemberFormControllerV2 implements ControllerV2 {
     @Override
     public Myview process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        return new Myview("WEB-INF/views/new-form.jsp");
+        return new Myview("/WEB-INF/views/new-form.jsp");
     }
 }
