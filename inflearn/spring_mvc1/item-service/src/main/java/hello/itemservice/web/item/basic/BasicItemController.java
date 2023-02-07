dd4e64ffcd1a68b76dc050c4e8ce7bb516a2497a
easydong02 <easydong02@gmail.com>
2023-02-07 12:24:38 +0900

    [2023-02-07] add item detail, item add

--- inflearn/spring_mvc1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ inflearn/spring_mvc1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -28,6 +28,54 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+//    @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        model.addAttribute("item",item);
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    //ModelAttribute는 Model객체도 포함, 알아서 넣어준다.
+    @PostMapping("/add")
+    public String saveV2(@ModelAttribute("item") Item item){
+
+//        model.addAttribute("item",item);
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+    //ModelAttribute는 Model객체도 포함, 알아서 넣어준다.
+//    @PostMapping("/add")
+    public String saveV3(@ModelAttribute Item item){
+
+//        model.addAttribute("item",item);
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+
     /**
      * 테스트용 데이터 추가
      */
