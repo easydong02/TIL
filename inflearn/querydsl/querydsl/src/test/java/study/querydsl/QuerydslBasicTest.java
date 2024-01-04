bbd31d2058e97295a643f59eb27f3099ebe177a2
easydong02 <easydong02@gmail.com>
2024-01-04 21:52:32 +0900

    [2024-01-04]queryDsl: querydsl - Q-Type

--- inflearn/querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ inflearn/querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,8 @@ import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
+import static study.querydsl.entity.QMember.member;
+
 @SpringBootTest
 @Transactional
 public class QuerydslBasicTest {
@@ -52,12 +54,10 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl(){
-        QMember m = new QMember("m");
-
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
