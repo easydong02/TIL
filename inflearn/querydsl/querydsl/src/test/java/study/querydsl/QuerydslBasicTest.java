322ec4d3caa89449d2c555c128469583097a69c4
dhshin <dhshin@inzisoft.com>
2024-01-05 09:23:21 +0900

    [2024-01-05]querydsl: 집합 - 에러 수정

--- inflearn/querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ inflearn/querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -12,13 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -193,5 +192,5 @@ public class QuerydslBasicTest {
         assertThat(teamA.get(team.name)).isEqualTo("teamA");
         assertThat(teamA.get(member.age.avg())).isEqualTo(15);
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
-        a
+    }
 }
