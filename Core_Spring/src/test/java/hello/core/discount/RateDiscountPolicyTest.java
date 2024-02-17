c2ce9aefd9eb81d99f47a1c8bcda055e66cee132
easydong02 <easydong02@gmail.com>
2024-02-18 01:13:36 +0900

    [2024-02-18]spring_core_1: DIP 적용

---
+++ Core_Spring/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,42 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10퍼 할인")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("vip아니면 할인되면 안 됨.")
+    void vip_x(){
+        //given
+        Member member = new Member(1L, "memberBasic", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
